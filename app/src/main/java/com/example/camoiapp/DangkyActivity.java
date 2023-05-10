489d135ce3345fbcde254772e7ad614a8bd17fa4
SuSuneee <susufgmn27@gmail.com>
2023-05-10 17:19:48 +0700

    First Commit

---
+++ app/src/main/java/com/example/camoiapp/DangkyActivity.java
@@ -0,0 +1,13 @@
+package com.example.camoiapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DangkyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_dangky);
+    }
+}
